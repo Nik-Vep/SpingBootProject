e786b2950f30d1836ca7e666d7d5d7f42fda60b3
nik_vep <nikitka1992.1992@mail.ru>
2024-11-27 21:07:57 +0300

    .

---
+++ src/main/java/ProjectBoot/dao/UserDAOImpl.java
@@ -0,0 +1,46 @@
+package ProjectBoot.dao;
+
+import ProjectBoot.model.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public class UserDAOImpl implements UserDAO{
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<User> allUsers() {
+        TypedQuery<User> query = entityManager.createQuery("FROM User", User.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public void add(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void delete(User user) {
+        if (entityManager.contains(user)) {
+            entityManager.remove(user);
+        } else {
+            entityManager.remove(entityManager.merge(user));
+        }
+    }
+
+    @Override
+    public void edit(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public User getById(int id) {
+        return entityManager.find(User.class, id);
+    }
+}
---
+++ src/main/java/ProjectBoot/service/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package ProjectBoot.service;
+
+import ProjectBoot.dao.UserDAO;
+import ProjectBoot.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService{
+
+    private UserDAO userDAO;
+
+    @Autowired
+    private void setUserDAO (UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    public List<User> allUsers() {
+        return userDAO.allUsers();
+    }
+
+    @Override
+    @Transactional
+    public void add(User user) {
+        userDAO.add(user);
+    }
+
+    @Override
+    @Transactional
+    public void delete(User user) {
+        userDAO.delete(user);
+    }
+
+    @Override
+    @Transactional
+    public void edit(User user) {
+        userDAO.edit(user);
+    }
+
+    @Override
+    public User getById(int id) {
+        return userDAO.getById(id);
+    }
+}
